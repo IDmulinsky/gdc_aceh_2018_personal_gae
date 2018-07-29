ea15cb8afe23923e7d04623b0dac9d26270a174f
Idham Kasyfi Mulinsky <idhamkasyfi1995@gmail.com>
2018-07-29 12:58:52 +0700

    aplikasi sudah bisa menampilkan hello-world

---
+++ helloworld.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title> PHP runs on GAE </title>
+    </head>
+
+    <body>
+        <?php echo "tes, 1, 2, 3"; ?>
+        <h1>PHP is almost cool!
+        </h1>
+
+        <img src= "http://img12.deviantart.net/6a7e/i/2011/288/7/6/deal_with_it___rainbow_style__by_j_brony-d4cwgad.png"
+        alt="Deal with it">
+
+    </body>
+</html>
